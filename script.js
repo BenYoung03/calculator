8fcf70276121e0ecd9924b2801a412a9dbdef376
BenYoung03 <bryoung1@lakeheadu.ca>
2023-08-01 19:38:50 -0230

    Add logic to the rest of the arithmetic operators and logic that stops user from dividing by 0

--- script.js
+++ script.js
@@ -19,13 +19,17 @@ function divide(a, b){
 
 function operate(firstNum, secondNum, operator){
     if(operator === '+'){
-        return add(firstNum, secondNum);
+        return add(firstNum, secondNum).toFixed(2);
     } else if (operator === '-'){
-        return subtract(firstNum, secondNum);
+        return subtract(firstNum, secondNum).toFixed(2);
     } else if (operator === '*'){
-        return multiply(firstNum, secondNum);
+        return multiply(firstNum, secondNum).toFixed(2);
     } else {
-        return divide(firstNum, secondNum);
+        if(secondNum === 0){ 
+            display.textContent = 'Cannot divide by 0';
+            return null;
+        }
+        return divide(firstNum, secondNum).toFixed(2);
     }
 }
 
@@ -47,6 +51,7 @@ buttons.forEach((button) => {
             result = null;
             operator = null;
         } else if(button.id === 'add'){
+            if(display.textContent === '') displayValue = '0';
             if(firstNum === null){
                 firstNum = parseInt(displayValue);
                 displayValue = '';
@@ -54,25 +59,72 @@ buttons.forEach((button) => {
                 operator = '+';
             } else {
                 let secondNum = parseInt(displayValue);
+                display.textContent = '';
                 firstNum = operate(firstNum, secondNum, operator);
-                display.textContent = ''; //Maybe change this to display the firstNum value
                 operator = '+';
                 displayValue = '';
             }
         } else if(button.id === 'subtract'){
-            // handle subtraction button
+            if(display.textContent === '') displayValue = '0';
+            if(firstNum === null){
+                firstNum = parseInt(displayValue);
+                displayValue = '';
+                display.textContent = displayValue;
+                operator = '-';
+            } else {
+                let secondNum = parseInt(displayValue);
+                display.textContent = ''; //Maybe change this to display the firstNum value
+                firstNum = operate(firstNum, secondNum, operator);    
+                operator = '-';
+                displayValue = '';
+            }
         } else if(button.id === 'multiply'){
-            // handle multiplication button
+            if(display.textContent === '') displayValue = '0';
+            if(firstNum === null){
+                firstNum = parseInt(displayValue);
+                displayValue = '';
+                display.textContent = displayValue;
+                operator = '*';
+            } else {
+                let secondNum = parseInt(displayValue);
+                display.textContent = ''; //Maybe change this to display the firstNum value
+                firstNum = operate(firstNum, secondNum, operator);
+                operator = '*';
+                displayValue = '';
+            }
         } else if(button.id === 'divide'){
-            // handle division button
+            if(display.textContent === '') displayValue = '0';
+            if(firstNum === null){
+                firstNum = parseInt(displayValue);
+                displayValue = '';
+                display.textContent = displayValue;
+                operator = '/';
+            } else {
+                let secondNum = parseInt(displayValue);
+                if(secondNum === 0){
+                    display.textContent = 'OOPSIE!';
+                    return;
+                }
+                display.textContent = ''; //Maybe change this to display the firstNum value
+                firstNum = operate(firstNum, secondNum, operator);
+                operator = '/';
+                displayValue = '';
+            }
         } else if (button.id === 'equals'){
             if(firstNum !== null && operator !== null){
                 let secondNum = parseInt(displayValue);
-                result = operate(firstNum, secondNum, operator);
-                displayValue = result.toString();
-                display.textContent = displayValue;
-                firstNum = null;
-                operator = null;
+                let result = operate(firstNum, secondNum, operator);
+                if(result === null){
+                    // Handle divide by 0 error
+                    displayValue = '';
+                    firstNum = null;
+                    operator = null;
+                } else {
+                    displayValue = result.toString();
+                    display.textContent = displayValue;
+                    firstNum = null;
+                    operator = null;
+                }
             }
         } else {
             displayValue += button.textContent;
